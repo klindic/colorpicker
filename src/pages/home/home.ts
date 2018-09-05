c28401a67c5fe1c66a35afd4ac19832d7e7c6776
kklindic <kristijan@euroart93.hr>
2018-09-05 15:44:43 +0200

    color picker setup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ColorPickerPage } from '../color-picker/color-picker';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+
+  goto() {
+    this.navCtrl.push(ColorPickerPage);
   }
 
 }
